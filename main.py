11738f56e86986b6ba95d75a70a30674b6ff5b04
Akubisa12 <allouisiusjessen@gmail.com>
2023-09-18 21:03:15 +0700

    first-commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+
+root = customtkinter.CTk()
+root.geometry("500x300")
+
+def login():
+    print("Test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20,padx=60,fill="both",expand=True)
+
+label = customtkinter.CTkLabel(master=frame,text="Login System",font=("Roboto",24))
+label.pack(pady=12,padx=10)
+
+
+entry1 = customtkinter.CTkEntry(master=frame,placeholder_text="Username")
+entry1.pack(pady=12,padx=10)
+
+
+entry2 = customtkinter.CTkEntry(master=frame,placeholder_text="Password",show="*")
+entry2.pack(pady=12,padx=10)
+
+button = customtkinter.CTkButton(master=frame,text="Login",command=login)
+button.pack(pady=12,padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame,text="Remember Me")
+checkbox.pack(pady=12,padx=10)
+
+root.mainloop()
